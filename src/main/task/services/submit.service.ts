3bb421d9d44597e5547e239f02ba180806a94413
arafat2020 <arafatmannan2019@gmail.com>
2025-06-27 08:44:27 +0600

    minor adjustment

---
+++ src/main/task/services/submit.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@nestjs/common';
+import { DbService } from 'src/utils/db/db.service';
+import { SubmitDto } from 'src/main/task/dto/submit.dto';
+import { FileService } from 'src/utils/file/file.service';
+import { FileInstance } from 'generated/prisma';
+import { IdDto } from 'src/common/dto/id.dto';
+import { ApiResponse } from 'src/common/types/apiResponse';
+
+@Injectable()
+export class SubmitService {
+  constructor(
+    private readonly db: DbService,
+    private readonly file: FileService,
+  ) {}
+
+  public async submit(
+    submitDto: SubmitDto,
+    { id }: IdDto,
+  ): Promise<ApiResponse<any>> {
+    const { signature } = submitDto;
+
+    let fileInstance: FileInstance | null = null;
+
+    if (signature)
+      fileInstance = await this.file.processUploadedFile(signature);
+
+    const taskSubmission = await this.db.serviceRequest.update({
+      where: { id },
+      data: {
+        rating: submitDto.rating,
+        review: submitDto.review,
+        ...(fileInstance && {
+          signature: { connect: { id: fileInstance.id } },
+        }),
+      },
+    });
+
+    return {
+      data: taskSubmission,
+      message: 'Task submitted successfully',
+      success: true,
+    };
+  }
+}
--- src/main/task/task.controller.ts
+++ src/main/task/task.controller.ts
@@ -1,4 +1,53 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  Query,
+  UploadedFile,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { ApiBearerAuth } from '@nestjs/swagger';
+import { IdDto } from 'src/common/dto/id.dto';
+import { Roles } from 'src/decorator/roles.decorator';
+import { RolesGuard } from 'src/guard/role.guard';
+import { CommonService } from './services/common.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { FileType, MulterService } from 'src/utils/lib/multer.service';
+import { SubmitDto } from './dto/submit.dto';
+import { SubmitService } from './services/submit.service';
 
 @Controller('task')
-export class TaskController {}
+@ApiBearerAuth()
+export class TaskController {
+  constructor(
+    private readonly commonService: CommonService,
+    private readonly submitService: SubmitService
+) {}
+
+  @UseGuards(AuthGuard('jwt'), RolesGuard)
+  @Roles('CLIENT')
+  @Post('details')
+  getTasks(@Query() id: IdDto) {
+    return this.commonService.getTaskDetails(id);
+  }
+
+  @UseGuards(AuthGuard('jwt'), RolesGuard)
+  @Roles('CLIENT')
+  @Post('submit')
+  @UseInterceptors(
+    FileInterceptor(
+      'signature',
+      new MulterService().createMulterOptions('./temp', 'temp', FileType.ANY),
+    ),
+  )
+  submit(@UploadedFile() signature: Express.Multer.File, @Body() body: SubmitDto, @Query() id: IdDto) {
+    const submitDto:SubmitDto = { 
+      rating: body.rating,
+      review: body.review,
+      signature
+     };
+    return this.submitService.submit(submitDto, id);
+  }
+}
